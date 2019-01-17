fb47e5eac232bd03a22a18884e5a8d6ca9e724cf
Lucas96Mireles <Lucas96mireles@gmail.com>
2019-01-17 15:30:58 -0500

    Seperate folders and task3.py

---
+++ Project/Task 3,4/Task3.py
@@ -0,0 +1,25 @@
+# Project  Task 3
+# Capt Arif Alamri 
+# Lt Micah Hayden
+# Lt Lucas Mireles
+
+import socket 
+import sys
+from scapy.all import *
+import time
+import random
+
+dest_ip = "10.12.1.141"		#mi1.m4i.local use nslookup to find IP
+dest_port = 2600		# UDP port 2600
+		# has to be 3,33,333,3333, or 33333
+source_ip = "10.204.0.68"	# my IP
+
+
+source_port = 33333	
+packets = []
+for i in range (9999):
+	message = "Ethan Hunt {0}".format(i)
+	packets.append( IP(dst=dest_ip,src=source_ip)/UDP(dport=dest_port, sport=source_port)/message)
+	
+send(packets, inter =0)
+
