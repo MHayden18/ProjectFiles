d5628a69dce3afd3d045d31878b4546e6445cec7
Micah Hayden <Micah.hayden@afit.edu>
2019-01-17 15:23:12 -0500

    Python script for checking port response times

--- Project/Task3_ResponseCheck.py
+++ Project/Task3_ResponseCheck.py
@@ -3,19 +3,23 @@
 # Lt Micah Hayden
 # Lt Lucas Mireles
 
-# This will send one attempted packet to each port to verify times
+# This will send one attempted packet to each port to verify response times
 import socket 
 import sys
 from scapy.all import *
 import time
 import random
 
-dest_ip = "10.12.1.1"
-dest_port = 1337
-source_ip = "10.204.0.67"
-message = "Deactivate!!"
+dest_ip = "10.12.1.141"
+dest_port = 2600
+source_ip = "10.204.0.68"
+
+source_ports = [3, 33, 333, 3333, 33333]
+message = "Ethan Hunt 0"
 packets = []
-for i in range (5018):
-	packets.append( IP(dst=dest_ip,src=source_ip)/UDP(dport=dest_port, sport=random.randint(1024,65535))/message)
-	
-send(packets, inter = 0)
+for item in source_ports:
+	packets.append( IP(dst=dest_ip,src=source_ip)/UDP(dport=dest_port, sport=item)/message)
+
+print("Sending packets")
+sr(packets, inter = 0)
+ans.summary()
